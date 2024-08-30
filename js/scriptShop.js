55ae18228af29eb107613d282c63d9714d1593df
Mohammed-Mujib <wadmujib@gmail.com>
2024-08-30 23:56:47 +0300

    display products and footer

---
+++ js/scriptShop.js
@@ -0,0 +1,72 @@
+const products = [
+    {
+        name:"package1",
+        image:"package2.jpg",
+        price:34,
+        addedToCart:false,
+    },
+    {
+        name:"package2",
+        image:"package3.jpg",
+        price:20,
+        addedToCart:false,
+    },
+    {
+        name:"package3",
+        image:"package4.jpg",
+        price:40,
+        addedToCart:false,
+    },
+    {
+        name:"package4",
+        image:"package5.jpg",
+        price:23,
+        addedToCart:false,
+    },
+    {
+        name:"package5",
+        image:"package6.jpg",
+        price:39,
+        addedToCart:false,
+    },
+    {
+        name:"package6",
+        image:"package7.jpg",
+        price:57,
+        addedToCart:false,
+    },
+    {
+        name:"package7",
+        image:"package8.jpg",
+        price:84,
+        addedToCart:false,
+    }
+];
+let cart = []
+const productsRows = document.querySelector("#productsRows");
+function displayProducts() {
+    let cartona = "";
+    for ( let product of products) {
+        cartona +=`
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 mt-2">
+                <div class="product_box position-relative">
+                    <div class="img_box mb-2 ">
+                        <img src="images/${product.image}" alt="" class=" w-100">
+                    </div>
+                    <span class="product_price px-4 py-2">$ ${product.price}</span>
+                    <div class="stars">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+                    <h2 class="text-center py-2 product_title">${product.name}</h2>
+                    <button class="btn text-center w-100 add_btn mt-1 mb-2 rounded-0 p-2"><i class="fa-solid fa-cart-shopping"></i> Add to cart</button>
+                </div>
+            </div>
+        `;
+    }
+    productsRows.innerHTML = cartona;
+}
+displayProducts()
